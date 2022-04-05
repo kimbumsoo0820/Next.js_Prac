14c0e17e29b6645277b94182481af2a948f57ab1
kimbumsoo0820 <kbs2082@naver.com>
2022-04-05 17:52:15 +0900

    style1

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,29 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/">
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
+      </Link>
+      <Link href="/about">
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/about" ? styles.active : ""
+          }`}
+        >
+          About
+        </a>
+      </Link>
     </nav>
   );
 }
